c02d1589c1f784473ee128c603bf0790d1acb4f1
Shashank-Handa <handa.shashank.36@gmail.com>
2022-03-13 10:49:48 +0530

    3

--- client.py
+++ client.py
@@ -1,14 +1,14 @@
 from http import client
 import socket
-serverName="10.0.0.1"
-serverPort="12000"
+serverName="10.0.2.5"
+serverPort=12000
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
 
-sentence=input("enter file path")
+sentence=input("enter file path :")
 
 clientSocket.send(sentence.encode())
 
 modified=clientSocket.recv(1024)
-print("from serverL ", modified.decode())
+print("from server: ", modified.decode())
 clientSocket.close()
