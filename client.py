f7ed3c2758d287fd8cdbf438d29e57b205a88ce5
Shashank-Handa <handa.shashank.36@gmail.com>
2022-03-14 10:10:38 +0530

    5

--- client.py
+++ client.py
@@ -2,13 +2,21 @@ from http import client
 import socket
 serverName="10.0.0.1"
 serverPort="12000"
-clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientSocket.connect((serverName, serverPort))
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clientSocket:
+    clientSocket.connect((serverName, serverPort))
 
-sentence=input("enter file path")
+    #accept file path and open file
+    filepath=input("enter file path")
+    i=0
+    while(i!=1):
+        try:
+            i=1
+            img=open(filepath, 'rb');
+        except:
+            print("file does not exist")
+            i=0
+    
+    clientSocket.send(filepath.encode())
+    clientSocket.send(img)
 
-clientSocket.send(sentence.encode())
-
-modified=clientSocket.recv(1024)
-print("from serverL ", modified.decode())
-clientSocket.close()
+    clientSocket.close()
