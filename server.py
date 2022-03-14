026ab8db392a47a62997142573a7941a8e3f2f37
Shashank-Handa <handa.shashank.36@gmail.com>
2022-03-14 17:58:24 +0530

    image transaction(server)

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+from os import remove
 serverPort = 12000
 serverSocket =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverSocket.bind(('',serverPort))
@@ -6,13 +7,21 @@ serverSocket.listen(1)
 print("Server is running")
 while True:
     connectionSocket, addr = serverSocket.accept()
-    filepath = connectionSocket.recv(1024).decode()
-
-    myfile=open(filepath+"1", "wb")
+    filepath=""
+    char=""
+    while(char!=">"):
+        filepath+=char
+        char = connectionSocket.recv(1).decode()
     
-    img=connectionSocket.recv(1024)
+    input(filepath)
 
-    myfile.write(img)
+    myfile=open("1"+filepath, "wb")
+    print("opened")
+
+    img=connectionSocket.recv(470000)
+    while(img!=b''): 
+        myfile.write(img)
+        img=connectionSocket.recv(470000)
     myfile.close()
-
+    print("done")
     connectionSocket.close()
