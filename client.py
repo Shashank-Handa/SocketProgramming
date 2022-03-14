9715efe053133e1d57e26ac406792bff4dbf7199
Shashank-Handa <handa.shashank.36@gmail.com>
2022-03-14 17:58:04 +0530

    image transaction

--- client.py
+++ client.py
@@ -1,22 +1,23 @@
 from http import client
+from pydoc import cli
 import socket
-serverName="10.0.0.1"
+serverName="10.0.2.5"
 serverPort=12000
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clientSocket:
-    clientSocket.connect((serverName, serverPort))
+clientSocket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientSocket.connect((serverName, serverPort))
+print("successfully connected")
+#accept file path and open file
+filepath=input("enter file path")
+i=0
+while(i!=1):
+    try:
+        i=1
+        imgfile=open(filepath, 'rb');
+    except:
+        print("file does not exist")
+        i=0
+img=imgfile.read()
 
-    #accept file path and open file
-    filepath=input("enter file path")
-    i=0
-    while(i!=1):
-        try:
-            i=1
-            img=open(filepath, 'rb');
-        except:
-            print("file does not exist")
-            i=0
-    
-    clientSocket.send(filepath.encode())
-    clientSocket.send(img)
+clientSocket.send((filepath+">").encode())
 
-    clientSocket.close()
+clientSocket.send(img)
