63ce45c6aa5bde64cec6bb678330c35c45ef05e4
Shashank-Handa <handa.shashank.36@gmail.com>
2022-03-14 16:36:13 +0530

    7

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import socket
 serverPort = 12000
-serverSocket =socket(socket.AF_INET, socket.SOCK_STREAM)
+serverSocket =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverSocket.bind(('',serverPort))
 serverSocket.listen(1)
 print("Server is running")
