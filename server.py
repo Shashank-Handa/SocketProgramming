a85991c4bcc96e768162fe7853dd462e9a30c003
Shashank-Handa <handa.shashank.36@gmail.com>
2022-03-15 14:17:40 +0530

    bleh

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 import socket
+from os import remove
+import PIL
 serverPort = 12000
 serverSocket =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverSocket.bind(('',serverPort))
@@ -8,11 +10,13 @@ while True:
     connectionSocket, addr = serverSocket.accept()
     filepath = connectionSocket.recv(1024).decode()
 
-    myfile=open(filepath+"1", "wb")
+    myfile=open("1"+filepath, "wb")
     
     img=connectionSocket.recv(1024)
 
     myfile.write(img)
     myfile.close()
+
+    imgfile=PIL.Image.open("1"+filepath)
 
     connectionSocket.close()
