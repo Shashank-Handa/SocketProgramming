e58ebaea60f4e7a08b80302e413e036897c25a7f
Shashank-Handa <handa.shashank.36@gmail.com>
2022-03-16 14:08:53 +0530

    formatted

--- client.py
+++ client.py
@@ -8,9 +8,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clientSocket:
     clientSocket.connect((serverName, serverPort))
     print("successfully connected")
     #accept file path and open file
-    filepath=input("enter file path")
+
     i=0
     while(i!=1):
+        filepath=input("enter file path")
         try:
             i=1
             imgfile=open(filepath, 'rb');
@@ -18,12 +19,17 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clientSocket:
             print("file does not exist")
             i=0
 
+    #read file
     img=imgfile.read()
     size1=len(img)
     charsize=str(size1)
+
+    #send image size in string format to server
     clientSocket.send((charsize+">").encode())
+    #send image to server
     clientSocket.sendall(img)
 
+    #recieve size of compressed image from server
     j=''
     charsize=''
     while(j!=">"):
@@ -31,6 +37,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clientSocket:
         charsize=charsize+j
     size=int(charsize[0:-1])
 
+    #recieve image from server and write to file
     with open("optimized"+filepath,"wb") as optimizedImage:
         tempbytes=b''
         img=b''
@@ -39,17 +46,11 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clientSocket:
             img=img+temp
 
         optimizedImage.write(img)
-    
-    """with open("optimized"+filepath,"wb") as optimizedImage:
-        img2=clientSocket.recv(470000)
-        while(img2!=b''): 
-            print("recieveloop")
-            optimizedImage.write(img2)
-            img2=clientSocket.recv(470000)"""
         
     optimizedImage.close()
-    img2=open("optimized"+filepath,"rb").read()
-    size2=len(img2)
+
+    #size of image
+    size2=len(img)
 
     print("size of image before compression: ", size1)
     print("size of image after compression: ", size2)
--- server.py
+++ server.py
@@ -3,51 +3,48 @@ import os
 import socketserver
 from imgcompression import compress
 import math
+
 serverPort = 12000
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSocket :
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSocket:
+
     socketserver.TCPServer.allow_reuse_address=True
     serverSocket.bind(('',serverPort))
     serverSocket.listen(1)
     print("Server is running")
+
     while True:
         connectionSocket, addr = serverSocket.accept()
+        print("connected to client ",addr)
         with connectionSocket:
+
+            #file to store image
             filepath="temp.jpg"
             filepath="1"+filepath
-            
-            print("opened")
+
+            #loop to recieve image size
             j=''
             charsize=''
             while(j!=">"):
                 j=connectionSocket.recv(1).decode()
                 charsize=charsize+j
             size=int(charsize[0:-1])
+            print("recieved image size: ", size)
+
+            #loop to recieve image
             with open(filepath, "wb") as myfile:
-                tempbytes=b''
                 img=b''
                 while(len(img)<size):
-                    
                     temp=connectionSocket.recv(128000)
                     img=img+temp
                 myfile.write(img)
-            
-    
-                    
-                    
-            """img=connectionSocket.recv(470000)
-            with open(filepath, "wb") as myfile:
-                while(img!=b''): 
-                    myfile.write(img)
-                    try:
-                        img=connectionSocket.recv(470000)
-                    except:
-                        break;
-                    print("revieve loop")   
-                print("loop exited")
-            myfile.close()
-            print("file closed")"""
-            filepath2=compress(filepath)
+            print("recieved image")
 
+            #Compressing image file
+            filepath2=compress(filepath)
+            print("image compressed")
+
+            #sending compressed image
             myfile=open(filepath2, "rb")
             img2=myfile.read()
             size=len(img2)
@@ -55,7 +52,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSocket :
             connectionSocket.send((charsize+">").encode())
             connectionSocket.sendall(img2)
 
-            print("done")
+            print("image file sent")
+            print("Closing connection with client")
+
         connectionSocket.close()
         os.remove(filepath)
         os.remove(filepath2)
