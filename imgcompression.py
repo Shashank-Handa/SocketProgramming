cbe0f0b9eaebd7ea408ba2c3b78889e57853e833
Shashank-Handa <handa.shashank.36@gmail.com>
2022-03-17 12:14:03 +0530

    close

--- imgcompression.py
+++ imgcompression.py
@@ -5,5 +5,6 @@ def compress(filepath):
     size=img.size
     img=img.resize((int(size[0]*0.8),int(size[1]*0.8)),Image.ANTIALIAS)
     img.save("2"+filepath,optimize=True,quality=80)
+    img.close()
 
     return "2"+filepath
